b0468ae56ca1b109c625e26f89410b7c21b11909
Muslih <muslihzarth@gmail.com>
2013-08-12 12:58:25 +0800

    registering plugin

---
+++ cloud9.php
@@ -0,0 +1,12 @@
+<?php 
+/*
+Plugin Name: Cloud9 ecommerce
+Plugin URI: https://github.com/muslih/cloud-9-wp-plugin/
+Description: cloud9 plugin ecommerce
+Version: 1.0
+Author: iwebcrux
+Author URI: http://iwebcrux.com
+License: GPLv2
+*/
+
+?>
